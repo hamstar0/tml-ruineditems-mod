ee9540ab0e8d07ac50f8f52ab8a212b5e2e1f030
hamstar0 <webutek@gmail.com>
2021-01-25 05:57:15 -0800

    v2.2.1 - Set ruined items to pulse * Set ruined items to pulse * Set only locked items to draw X overlay * Restructured item tooltip code files

--- RuinedItems/MyItem_Draw.cs
+++ RuinedItems/MyItem_Draw.cs
@@ -1,73 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.ModLoader;
+using HamstarHelpers.Helpers.Debug;
 using RuinedItems.Prefixes;
 
 
 namespace RuinedItems {
 	partial class RuinedItemsItem : GlobalItem {
-		public override void ModifyTooltips( Item item, List<TooltipLine> tooltips ) {
-			if( item.prefix != ModContent.PrefixType<RuinedPrefix>() ) {
-				return;
-			}
-			if( item.accessory ) {
-				this.AddRuinedAccessoryTooltips( item, tooltips );
-			}
+		public override Color? GetAlpha( Item item, Color lightColor ) {
+			if( item.prefix == ModContent.PrefixType<RuinedPrefix>() ) {
+				float pulse = (float)Main.mouseTextColor / 255f;
+				pulse = pulse * pulse * pulse;
 
-			var tip = new TooltipLine( this.mod, "RuinedPrefixDesc", "Item is ruined and will need to be reforged" );
-			tip.overrideColor = Color.Red;
-			tooltips.Add( tip );
-		}
-
-		////
-
-		private void AddRuinedAccessoryTooltips( Item item, List<TooltipLine> tooltips ) {
-			int statNum = 0;
-
-			//
-
-			void addTip( string stat, bool isBad ) {
-				var tip = new TooltipLine( this.mod, "RuinedPrefixAccStats_"+statNum, stat );
-
-				if( isBad ) {
-					tip.overrideColor = new Color( 190, 120, 120 );
-				} else {
-					tip.overrideColor = new Color( 120, 190, 120 ) ;
-				}
-
-				tooltips.Add( tip );
-
-				statNum++;
+				Color newColor = lightColor * pulse;
+				newColor.A = lightColor.A;
+				return newColor;
 			}
 
-			//
-
-			var config = RuinedItemsConfig.Instance;
-			//int critAdd = config.Get<int>( nameof(config.RuinedCritAdd) );
-			float moveSpeedMul = config.Get<float>( nameof(config.RuinedAccessoryMoveSpeedScale) );
-			float meleeSpeedAdd = config.Get<float>( nameof(config.RuinedAccessoryMeleeSpeedAdd) );
-			int defAdd = config.Get<int>( nameof(config.RuinedAccessoryDefenseAdd) );
-
-			int moveSpeedPerc = (int)((moveSpeedMul - 1f) * 100f);
-			int meleeSpeedPerc = (int)(meleeSpeedAdd * 100f);
-
-			//addTip( critAdd+"% crit chance" );
-			addTip( moveSpeedPerc+"% movement speed", moveSpeedPerc < 0 );
-			addTip( meleeSpeedPerc+"% melee speed", meleeSpeedPerc < 0 );
-			addTip( defAdd+" defense", defAdd < 0 );
+			return base.GetAlpha( item, lightColor );
 		}
 
 
-		////////////////
-
-		public override void PostDrawInInventory( Item item, SpriteBatch sb, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale ) {
+		public override void PostDrawInInventory(
+					Item item,
+					SpriteBatch sb,
+					Vector2 position,
+					Rectangle frame,
+					Color drawColor,
+					Color itemColor,
+					Vector2 origin,
+					float scale ) {
 			if( item == null || item.IsAir ) { return; }
 			if( item.prefix != ModContent.PrefixType<RuinedPrefix>() ) { return; }
 
+			var config = RuinedItemsConfig.Instance;
 			var mymod = (RuinedItemsMod)this.mod;
+
+			if( !config.Get<bool>(nameof(config.RuinedItemsLockedFromUse)) ) {
+				return;
+			}
 
 			float posX = position.X;
 			posX += ((float)frame.Width / 2f) * scale;
